5c58b1c8f9b7207a3ee4c98ae054a49d16e71276
paulodhiambo <44492906+paulodhiambo@users.noreply.github.com>
2020-03-01 19:34:51 +0300

    kotlin coroutines

--- app/src/main/java/com/odhiambopaul/movies/network/TmdbEndpoints.kt
+++ app/src/main/java/com/odhiambopaul/movies/network/TmdbEndpoints.kt
@@ -1,12 +1,15 @@
 package com.odhiambopaul.movies.network
 
-import io.reactivex.Observable
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface TmdbEndpoints {
 
+//    @GET("/3/movie/popular")
+//    fun getMovies(@Query("api_key") key: String): Observable<PopularMovies>
+
     @GET("/3/movie/popular")
-    fun getMovies(@Query("api_key") key: String): Observable<PopularMovies>
+    suspend fun getMovies(@Query("api_key") key: String): Response<PopularMovies>
 
 }
--- app/src/main/java/com/odhiambopaul/movies/ui/home/MainActivity.kt
+++ app/src/main/java/com/odhiambopaul/movies/ui/home/MainActivity.kt
@@ -5,26 +5,50 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.odhiambopaul.movies.network.PopularMovies
 import com.odhiambopaul.movies.R
+import com.odhiambopaul.movies.network.PopularMovies
 import com.odhiambopaul.movies.network.ServiceBuilder
 import com.odhiambopaul.movies.util.api_key
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val compositeDisposable = CompositeDisposable()
-        compositeDisposable.add(
-            ServiceBuilder.buildService().getMovies(api_key)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe({response -> onResponse(response)}, {t -> onFailure(t) }))
+//        val compositeDisposable = CompositeDisposable()
+//        compositeDisposable.add(
+//            ServiceBuilder.buildService().getMovies(api_key)
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribeOn(Schedulers.io())
+//                .subscribe({response -> onResponse(response)}, {t -> onFailure(t) })
+        CoroutineScope(Dispatchers.IO).launch {
+            val response=ServiceBuilder.buildService().getMovies(api_key)
+            withContext(Dispatchers.Main){
+                try {
+                    if(response.isSuccessful)
+                    {
+                        onResponse(response.body()!!)
+                    }
+                    else{
+                        Toast.makeText(this@MainActivity, "Error: ${response.code()}",Toast.LENGTH_SHORT).show()
+                    }
+                }
+                catch (e: HttpException)
+                {
+                    onFailure(e)
+                }
+                catch (e: Throwable)
+                {
+                    onFailure(e)
+                }
+            }
+        }
 
     }
 
